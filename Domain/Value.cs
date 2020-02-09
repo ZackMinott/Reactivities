d798749df137677d0cccd6bd90d45f319689345f
ZackMinott <zacharyminott1997@gmail.com>
2020-02-09 10:17:49 -0800

    Initial Commit

---
+++ Domain/Value.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Domain
+{
+    public class Value
+    {
+        // These are referred to as auto-implemented properties
+        public int Id {get; set;} 
+        public string Name {get; set;}
+    }
+}
